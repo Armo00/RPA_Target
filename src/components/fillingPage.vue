fc5f1f2c6dfb8c79f2c52c780e9bf94fa58f41fb
义人 周 <2654988228@qq.com>
2021-03-05 16:02:26 +0800

    ticket page build #2

--- src/components/fillingPage.vue
+++ src/components/fillingPage.vue
@@ -52,7 +52,7 @@
                     <el-col :span="6">
                         <el-input v-model="ticketFormData.incoterms" style="width: 100%;"></el-input>
                     </el-col>
-                   
+
                 </el-form-item>
 
                 <el-form-item label="起运国信息">
@@ -62,45 +62,25 @@
                     <el-input type="textarea" v-model="ticketFormData.toAddress" :rows="4"></el-input>
                 </el-form-item>
 
-                <el-form-item label="活动区域">
-                    <el-select v-model="form.region" placeholder="请选择活动区域">
-                        <el-option label="区域一" value="shanghai"></el-option>
-                        <el-option label="区域二" value="beijing"></el-option>
-                    </el-select>
-                </el-form-item>
-                <el-form-item label="活动时间">
-                    <el-col :span="11">
-                        <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-                    </el-col>
-                    <el-col class="line" :span="2">-</el-col>
-                    <el-col :span="11">
-                        <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
-                    </el-col>
-                </el-form-item>
-                <el-form-item label="即时配送">
-                    <el-switch v-model="form.delivery"></el-switch>
-                </el-form-item>
-                <el-form-item label="活动性质">
-                    <el-checkbox-group v-model="form.type">
-                        <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-                        <el-checkbox label="地推活动" name="type"></el-checkbox>
-                        <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-                        <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-                    </el-checkbox-group>
-                </el-form-item>
-                <el-form-item label="特殊资源">
-                    <el-radio-group v-model="form.resource">
-                        <el-radio label="线上品牌商赞助"></el-radio>
-                        <el-radio label="线下场地免费"></el-radio>
-                    </el-radio-group>
-                </el-form-item>
-                <el-form-item label="活动形式">
-                    <el-input type="textarea" v-model="form.desc"></el-input>
+                <el-form-item v-for="(item, index) in ticketFormData.importItems"
+                              :label="'进口货物' + index">
+                    <el-input v-model="item.materialDescription" placeholder="料号" class="cargoInput"></el-input>
+                    <el-input v-model="item.ogCountry" placeholder="原产国" class="cargoInput"></el-input>
+                    <el-input v-model="item.HSTNumber" placeholder="HS归类参考" class="cargoInput"></el-input>
+                    <el-input v-model="item.quantity" placeholder="数量" class="cargoInput"></el-input>
+                    <el-input v-model="item.unitPrice" placeholder="单价" class="cargoInput"></el-input>
+                    <el-input v-model="item.extendedPrice" placeholder="单个料号总价" class="cargoInput"></el-input>
+                    <el-input v-model="item.ECCN" placeholder="出口管制分类号码" class="cargoInput"></el-input>
+                    <el-input v-model="item.license" placeholder="许可证" class="cargoInput"></el-input>
+                    <el-date-picker type="date" placeholder="有效期" v-model="item.valid" class="cargoInput"></el-date-picker>
+
+                    <el-button @click.prevent="removeItem(item)" type="danger" round>删除</el-button>
                 </el-form-item>
                 <el-form-item>
-                    <el-button type="primary">立即创建</el-button>
-                    <el-button>取消</el-button>
+                    <el-button @click="addItem">新增货物</el-button>
+                    <el-button type="primary" @click="placeholder()">提交</el-button>
                 </el-form-item>
+
             </el-form>
 
 
@@ -125,6 +105,7 @@
 
                 multipleSelection: [],
                 dialogVisibleFlag: false,
+
                 form: {
                     name: '',
                     region: '',
@@ -143,6 +124,9 @@
                     importDate:'',
                     salesOrderNo: '',
                     incoterms: '',
+                    importItems: [
+                        { materialDescription: '', ogCountry: '', HTSNumber: '', quantity: '', unitPrice: '', extendedPrice: '', ECCN: '', license: '', valid: '' },
+                    ],
                     
                 },
             }
@@ -155,7 +139,17 @@
             },
             placeholder() {
                 console.log('rua');
-            }
+            },
+            removeItem(item) {
+                var index = this.ticketFormData.importItems.indexOf(item)
+                if (index !== -1) {
+                    this.ticketFormData.importItems.splice(index, 1)
+                };
+            },
+            addItem() {
+                var item = { materialDescription: '', ogCountry: '', HTSNumber: '', quantity: '', unitPrice: '', extendedPrice: '', ECCN: '', license: '', valid: '' };
+                this.ticketFormData.importItems.push(item);
+            },
             
 
 
@@ -191,6 +185,11 @@
         margin-left: 10px;
         width: 40%;
         float:left;
+    }
+    .cargoInput{
+        width:15%;
+        float:left;
+        margin-right:10px;
     }
 
 </style>
